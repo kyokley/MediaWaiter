8ba5af962636c6bea62a6bac58384a44de24568a
Kevin Yokley <kyokley2@gmail.com>
2015-12-05 16:25:43 -0600

    Removing obsolete ssl ciphers

--- fabfile.py
+++ fabfile.py
@@ -62,15 +62,15 @@ server {{
     access_log  /var/log/nginx/access.log;
     error_log  /var/log/nginx/error.log;
 
-	ssl on;
-	ssl_certificate {installDir}/server/server.crt;
-	ssl_certificate_key {installDir}/server/server.key;
+    ssl on;
+    ssl_certificate {installDir}/server/server.crt;
+    ssl_certificate_key {installDir}/server/server.key;
 
-	ssl_session_timeout 5m;
+    ssl_session_timeout 5m;
 
     ssl_protocols        TLSv1 TLSv1.1 TLSv1.2;
-    ssl_ciphers          HIGH:!aNULL:!MD5;
-	ssl_prefer_server_ciphers on;
+    ssl_ciphers "EECDH+ECDSA+AESGCM:EECDH+aRSA+AESGCM:EECDH+ECDSA+SHA256:EECDH+aRSA+SHA256:EECDH+ECDSA+SHA384:EECDH+ECDSA+SHA256:EECDH+aRSA+SHA384:EDH+aRSA+AESGCM:EDH+aRSA+SHA256:EDH+aRSA:EECDH:!aNULL:!eNULL:!MEDIUM:!LOW:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS:!RC4:!SEED";
+    ssl_prefer_server_ciphers on;
 
     location /assets {{
         alias {installDir}/static/assets;
