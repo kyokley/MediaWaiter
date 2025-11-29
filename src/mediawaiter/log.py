4aee0d9545443c71c7a08b84a99d9e1714d99268
Kevin Yokley <kyokley@mars>
2025-11-28 22:36:02 -0600

    Remove file based logging handler

--- src/mediawaiter/log.py
+++ src/mediawaiter/log.py
@@ -1,6 +1,5 @@
 import logging
 
-from logging.handlers import RotatingFileHandler
 from .settings import LOG_FILE_NAME, LOG_PATH
 
 fullLogPath = LOG_PATH / LOG_FILE_NAME
@@ -19,19 +18,8 @@ class LogFile:
     @classmethod
     def getLogger(cls):
         if not cls.logger:
-            formatter = logging.Formatter(
-                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-            )
             log = logging.getLogger("waiter")
             log.setLevel(logging.DEBUG)
-            rfh = RotatingFileHandler(
-                fullLogPath,
-                mode="a",
-                maxBytes=1000000,
-                backupCount=10,
-            )
-            rfh.setFormatter(formatter)
-            log.addHandler(rfh)
             cls.logger = log
 
         return cls.logger
