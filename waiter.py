225a293ff20dd62f3f8e6e667149931c5690af11
Kevin Yokley <kyokley2@gmail.com>
2026-08-13 06:59:35 -0500

    ♻️ refactor(main)(waiter): remove debug logging of token API response
    - remove logger().debug and print statements in getTokenByGUID
    - stop dumping full API response data to logs and stdout

--- waiter.py
+++ waiter.py
@@ -125,8 +125,6 @@ def getTokenByGUID(guid):
             timeout=REQUESTS_TIMEOUT,
         )
         data = resp.json()
-        logger().debug(str(data))
-        print(str(data))
         return data
     except Exception as e:
         logger().error(e)
